461e85244b14bd3e9512098cc3c27c28c4fe2d4c
Hsiaotzuu <141395476+Hsiaotzuu@users.noreply.github.com>
2023-08-07 14:51:38 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,211 @@
+import argparse
+import os
+from functools import partial
+
+import numpy as np
+import torch
+import torch.multiprocessing as mp
+import torch.nn.parallel
+
+from networks.Swinunetr import SwinUNETR_EPA
+from optimizers.lr_scheduler import LinearWarmupCosineAnnealingLR
+from trainer import run_training
+from utils.data_utils import get_loader
+
+from monai.inferers import sliding_window_inference
+from monai.losses import DiceCELoss
+from monai.metrics import DiceMetric
+from monai.transforms import Activations, AsDiscrete, Compose
+from monai.utils.enums import MetricReduction
+
+
+parser = argparse.ArgumentParser(description="EPA_Swin UNETR segmentation pipeline")
+parser.add_argument("--checkpoint", default=None, help="start training from saved checkpoint")
+parser.add_argument("--logdir", default="test", type=str, help="directory to save the tensorboard logs")
+parser.add_argument("--data_dir", default="./dataset/", type=str, help="dataset directory")
+parser.add_argument("--json_list", default="dataset.json", type=str, help="dataset json file")
+parser.add_argument("--pretrained_dir", default=" ", type=str, help="pretrained checkpoint directory")
+parser.add_argument("--pretrained_model_name",default="model.pt",type=str,help="pretrained model name")
+parser.add_argument("--save_checkpoint", action="store_true", help="save checkpoint during training")
+parser.add_argument("--max_epochs", default=4000, type=int, help="max number of training epochs")
+parser.add_argument("--batch_size", default=1, type=int, help="number of batch size")
+parser.add_argument("--sw_batch_size", default=4, type=int, help="number of sliding window batch size")
+parser.add_argument("--optim_lr", default=1e-4, type=float, help="optimization learning rate")
+parser.add_argument("--optim_name", default="adamw", type=str, help="optimization algorithm")
+parser.add_argument("--reg_weight", default=1e-5, type=float, help="regularization weight")
+parser.add_argument("--momentum", default=0.99, type=float, help="momentum")
+parser.add_argument("--noamp", action="store_true", help="do NOT use amp for training")
+parser.add_argument("--val_every", default=100, type=int, help="validation frequency")
+parser.add_argument("--rank", default=0, type=int, help="node rank for distributed training")
+parser.add_argument("--workers", default=8, type=int, help="number of workers")
+parser.add_argument("--in_channels", default=1, type=int, help="number of input channels")
+parser.add_argument("--out_channels", default=16, type=int, help="number of output channels")
+parser.add_argument("--feature_size", default=24, type=int, help="feature size")
+parser.add_argument("--a_min", default=-175.0, type=float, help="a_min in ScaleIntensityRanged")
+parser.add_argument("--a_max", default=250.0, type=float, help="a_max in ScaleIntensityRanged")
+parser.add_argument("--b_min", default=0.0, type=float, help="b_min in ScaleIntensityRanged")
+parser.add_argument("--b_max", default=1.0, type=float, help="b_max in ScaleIntensityRanged")
+parser.add_argument("--space_x", default=1.5, type=float, help="spacing in x direction")
+parser.add_argument("--space_y", default=1.5, type=float, help="spacing in y direction")
+parser.add_argument("--space_z", default=2.0, type=float, help="spacing in z direction")
+parser.add_argument("--roi_x", default=96, type=int, help="roi size in x direction")
+parser.add_argument("--roi_y", default=96, type=int, help="roi size in y direction")
+parser.add_argument("--roi_z", default=96, type=int, help="roi size in z direction")
+parser.add_argument("--infer_overlap", default=0.5, type=float, help="sliding window inference overlap")
+parser.add_argument("--lrschedule", default="warmup_cosine", type=str, help="type of learning rate scheduler")
+parser.add_argument("--dropout_rate", default=0.0, type=float, help="dropout rate")
+parser.add_argument("--dropout_path_rate", default=0.0, type=float, help="drop path rate")
+parser.add_argument("--RandFlipd_prob", default=0.2, type=float, help="RandFlipd aug probability")
+parser.add_argument("--RandRotate90d_prob", default=0.2, type=float, help="RandRotate90d aug probability")
+parser.add_argument("--RandScaleIntensityd_prob", default=0.1, type=float, help="RandScaleIntensityd aug probability")
+parser.add_argument("--RandShiftIntensityd_prob", default=0.1, type=float, help="RandShiftIntensityd aug probability")
+parser.add_argument("--warmup_epochs", default=50, type=int, help="number of warmup epochs")
+parser.add_argument("--resume_ckpt", action="store_true", help="resume training from pretrained checkpoint")
+parser.add_argument("--smooth_dr", default=1e-6, type=float, help="constant added to dice denominator to avoid nan")
+parser.add_argument("--smooth_nr", default=0.0, type=float, help="constant added to dice numerator to avoid zero")
+parser.add_argument("--use_checkpoint", action="store_true", help="use gradient checkpointing to save memory")
+parser.add_argument("--use_ssl_pretrained", action="store_true", help="use self-supervised pretrained weights")
+parser.add_argument("--spatial_dims", default=3, type=int, help="spatial dimension of input data")
+parser.add_argument("--squared_dice", action="store_true", help="use squared Dice")
+
+
+def main():
+    args = parser.parse_args()
+    args.amp = not args.noamp
+    args.logdir = "./runs/" + args.logdir
+    main_worker(gpu=0, args=args)
+
+
+def main_worker(gpu, args):
+
+    torch.cuda.set_device(args.gpu)
+    torch.backends.cudnn.benchmark = True
+    args.test_mode = False
+    loader = get_loader(args)
+    print(args.rank, " gpu", args.gpu)
+    
+    if args.rank == 0:
+        print("Batch size is:", args.batch_size, "epochs", args.max_epochs)
+    inf_size = [args.roi_x, args.roi_y, args.roi_z]
+
+    pretrained_dir = args.pretrained_dir
+
+    model = SwinUNETR_EPA(
+        img_size=(args.roi_x, args.roi_y, args.roi_z),
+        in_channels=args.in_channels,
+        out_channels=args.out_channels,
+        feature_size=args.feature_size,
+        drop_rate=0.0,
+        attn_drop_rate=0.0,
+        dropout_path_rate=args.dropout_path_rate,
+        use_checkpoint=args.use_checkpoint,
+    )
+
+    if args.resume_ckpt:
+        model_dict = torch.load(os.path.join(pretrained_dir, args.pretrained_model_name))
+        model.load_state_dict(model_dict,strict=False)
+        print("Use pretrained weights")
+        print("---------------------------------------------------------")
+
+    if args.use_ssl_pretrained:
+        try:
+            model_dict = torch.load("./pretrained_models/")
+            state_dict = model_dict["state_dict"]
+            # fix potential differences in state dict keys from pre-training to
+            # fine-tuning
+            if "module." in list(state_dict.keys())[0]:
+                print("Tag 'module.' found in state dict - fixing!")
+                for key in list(state_dict.keys()):
+                    state_dict[key.replace("module.", "")] = state_dict.pop(key)
+            if "swin_vit" in list(state_dict.keys())[0]:
+                print("Tag 'swin_vit' found in state dict - fixing!")
+                for key in list(state_dict.keys()):
+                    state_dict[key.replace("swin_vit", "swinViT")] = state_dict.pop(key)
+            # We now load model weights, setting param `strict` to False, i.e.:
+            # this load the encoder weights (Swin-ViT, SSL pre-trained), but leaves
+            # the decoder weights untouched (CNN UNet decoder).
+            model.load_state_dict(state_dict, strict=False)
+            print("Using pretrained self-supervised Swin UNETR backbone weights !")
+        except ValueError:
+            raise ValueError("Self-supervised pre-trained weights not available for" + str(args.model_name))
+
+    if args.squared_dice:
+        dice_loss = DiceCELoss(
+            to_onehot_y=True, softmax=True, squared_pred=True, smooth_nr=args.smooth_nr, smooth_dr=args.smooth_dr
+        )
+    else:
+        dice_loss = DiceCELoss(to_onehot_y=True, softmax=True)
+        
+    post_label = AsDiscrete(to_onehot=args.out_channels, n_classes=args.out_channels)
+    post_pred = AsDiscrete(argmax=True, to_onehot=args.out_channels, n_classes=args.out_channels) 
+    dice_acc = DiceMetric(include_background=True, reduction=MetricReduction.MEAN, get_not_nans=True)
+    model_inferer = partial(
+        sliding_window_inference,
+        roi_size=inf_size,
+        sw_batch_size=args.sw_batch_size,
+        predictor=model,
+        overlap=args.infer_overlap,
+    )
+
+    pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print("Total parameters count", pytorch_total_params)
+
+    best_acc = 0
+    start_epoch = 0
+
+    if args.checkpoint is not None:
+        checkpoint = torch.load(args.checkpoint, map_location="cpu")
+        from collections import OrderedDict
+
+        new_state_dict = OrderedDict()
+        for k, v in checkpoint["state_dict"].items():
+            new_state_dict[k.replace("backbone.", "")] = v
+        model.load_state_dict(new_state_dict, strict=False)
+        if "epoch" in checkpoint:
+            start_epoch = checkpoint["epoch"]
+        if "best_acc" in checkpoint:
+            best_acc = checkpoint["best_acc"]
+        print("=> loaded checkpoint '{}' (epoch {}) (bestacc {})".format(args.checkpoint, start_epoch, best_acc))
+
+    model.cuda(args.gpu)
+
+    if args.optim_name == "adam":
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.optim_lr, weight_decay=args.reg_weight)
+    elif args.optim_name == "adamw":
+        optimizer = torch.optim.AdamW(model.parameters(), lr=args.optim_lr, weight_decay=args.reg_weight)
+    elif args.optim_name == "sgd":
+        optimizer = torch.optim.SGD(
+            model.parameters(), lr=args.optim_lr, momentum=args.momentum, nesterov=True, weight_decay=args.reg_weight
+        )
+    else:
+        raise ValueError("Unsupported Optimization Procedure: " + str(args.optim_name))
+
+    if args.lrschedule == "warmup_cosine":
+        scheduler = LinearWarmupCosineAnnealingLR(
+            optimizer, warmup_epochs=args.warmup_epochs, max_epochs=args.max_epochs
+        )
+    elif args.lrschedule == "cosine_anneal":
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.max_epochs)
+        if args.checkpoint is not None:
+            scheduler.step(epoch=start_epoch)
+    else:
+        scheduler = None
+    accuracy = run_training(
+        model=model,
+        train_loader=loader[0],
+        val_loader=loader[1],
+        optimizer=optimizer,
+        loss_func=dice_loss,
+        acc_func=dice_acc,
+        args=args,
+        model_inferer=model_inferer,
+        scheduler=scheduler,
+        start_epoch=start_epoch,
+        post_label=post_label,
+        post_pred=post_pred,
+    )
+    return accuracy
+
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,167 @@
+import argparse
+import os
+import json
+
+import nibabel as nib
+import numpy as np
+import torch
+from networks.Swinunetr import SwinUNETR_EPA
+from utils.data_utils import get_loader
+from utils.utils import resample_3d, dice, HD
+from monai.inferers import sliding_window_inference
+
+parser = argparse.ArgumentParser(description="Swin UNETR segmentation pipeline")
+parser.add_argument("--pretrained_dir", default="./runs/", type=str, help="pretrained checkpoint directory")
+parser.add_argument("--pretrained_model_name",default="model.pt",type=str,help="pretrained model name",)
+parser.add_argument("--data_dir", default=" ", type=str, help="dataset directory") 
+parser.add_argument("--json_list", default="dataset_1.json", type=str, help="dataset json file")
+parser.add_argument("--exp_name", default="test1", type=str, help="experiment name")
+parser.add_argument("--feature_size", default=24, type=int, help="feature size")
+parser.add_argument("--infer_overlap", default=0.5, type=float, help="sliding window inference overlap")
+parser.add_argument("--in_channels", default=1, type=int, help="number of input channels")
+parser.add_argument("--out_channels", default=16, type=int, help="number of output channels") #
+parser.add_argument("--a_min", default=-175.0, type=float, help="a_min in ScaleIntensityRanged")
+parser.add_argument("--a_max", default=250.0, type=float, help="a_max in ScaleIntensityRanged")
+parser.add_argument("--b_min", default=0.0, type=float, help="b_min in ScaleIntensityRanged")
+parser.add_argument("--b_max", default=1.0, type=float, help="b_max in ScaleIntensityRanged")
+
+parser.add_argument("--space_x", default=1.5, type=float, help="spacing in x direction") #1.5
+parser.add_argument("--space_y", default=1.5, type=float, help="spacing in y direction") #1.5
+parser.add_argument("--space_z", default=2.0, type=float, help="spacing in z direction") #2.0
+
+parser.add_argument("--roi_x", default=96, type=int, help="roi size in x direction") #(1)96 (2)64
+parser.add_argument("--roi_y", default=96, type=int, help="roi size in y direction")
+parser.add_argument("--roi_z", default=96, type=int, help="roi size in z direction")
+
+parser.add_argument("--dropout_rate", default=0.0, type=float, help="dropout rate")
+parser.add_argument("--workers", default=8, type=int, help="number of workers")
+parser.add_argument("--RandFlipd_prob", default=0.2, type=float, help="RandFlipd aug probability")
+parser.add_argument("--RandRotate90d_prob", default=0.2, type=float, help="RandRotate90d aug probability")
+parser.add_argument("--RandScaleIntensityd_prob", default=0.1, type=float, help="RandScaleIntensityd aug probability")
+parser.add_argument("--RandShiftIntensityd_prob", default=0.1, type=float, help="RandShiftIntensityd aug probability")
+parser.add_argument("--spatial_dims", default=3, type=int, help="spatial dimension of input data")
+parser.add_argument("--use_checkpoint", action="store_true", help="use gradient checkpointing to save memory")
+
+
+def main():
+    args = parser.parse_args()
+    args.test_mode = True
+
+    val_loader = get_loader(args)
+    pretrained_dir = args.pretrained_dir
+    model_name = args.pretrained_model_name
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    pretrained_pth = os.path.join(pretrained_dir, model_name)
+    
+    print("load path : ", pretrained_pth)
+    
+    model = SwinUNETR_EPA(
+        img_size=96,
+        in_channels=args.in_channels,
+        out_channels=args.out_channels,
+        feature_size=args.feature_size,
+        drop_rate=0.0,
+        attn_drop_rate=0.0,
+        dropout_path_rate=0.0,
+        use_checkpoint=args.use_checkpoint,
+    )
+    
+    model_dict = torch.load(pretrained_pth)
+    
+    print("keys = " , model_dict.keys())
+    model.load_state_dict(model_dict["state_dict"])
+    
+    model.eval()
+    model.to(device)
+
+    pathh = os.path.join(args.data_dir , args.json_list)
+    with open(pathh) as f:
+        p = json.load(f)
+        
+        label_key = p["labels"]
+
+    with torch.no_grad():
+        dice_list_case = []
+        dice_list_file = []
+        
+        HD_list_case = []
+        HD_list_file = []
+        
+        for i, batch in enumerate(val_loader):
+            val_inputs, val_labels = (batch["image"].cuda(), batch["label"].cuda())
+            original_affine = batch["label_meta_dict"]["affine"][0].numpy()
+            _, _, h, w, d = val_labels.shape
+            target_shape = (h, w, d)
+            
+            img_name = batch["image_meta_dict"]["filename_or_obj"][0].split("\\")[-1]
+            print("Inference on case {}".format(img_name))
+            val_outputs = sliding_window_inference(
+                val_inputs, (args.roi_x, args.roi_y, args.roi_z), 4, model, overlap=args.infer_overlap, mode="gaussian"
+            )
+            
+            val_outputs = torch.softmax(val_outputs, 1).cpu().numpy()
+            val_outputs = np.argmax(val_outputs, axis=1).astype(np.uint8)[0]
+            val_labels = val_labels.cpu().numpy()[0, 0, :, :, :]
+            
+            print("target_shape", target_shape)
+            print("val_outputs.shape", val_outputs.shape)
+            
+            val_outputs = resample_3d(val_outputs, target_shape)
+            
+            save_path = os.path.join("./outputs/", img_name)
+            nib.save(
+                nib.Nifti1Image(val_outputs.astype(np.uint8), original_affine), save_path)
+            print("save output: {}".format(save_path))
+            
+            
+            #===DICE===================================================
+            dice_list_sub = []
+            for i in range(1, args.out_channels):
+                organ_Dice = dice(val_outputs == i, val_labels == i)
+                dice_list_sub.append(organ_Dice)
+            
+            for idx, per_dice_value in enumerate(dice_list_sub):
+                print(f"({label_key[str(idx+1)]}) = {per_dice_value:2.5f}")
+ 
+            mean_dice = np.mean(dice_list_sub)
+            print("Mean Organ Dice: {}".format(mean_dice))
+            print("-----------------------------------------")
+            dice_list_file.append(dice_list_sub)
+            dice_list_case.append(mean_dice)
+            
+            #===HD===================================================
+            HD_list_sub = []
+            for i in range(1, args.out_channels):
+                organ_HD = HD(val_outputs == i, val_labels == i)
+                HD_list_sub.append(organ_HD)
+            
+            for idx_2 , per_HD_value in enumerate(HD_list_sub):
+                print(f"({label_key[str(idx_2+1)]}) = {per_HD_value:2.5f}")  
+                
+            mean_HD = np.mean(HD_list_sub)
+            print("Mean Organ HD: {}".format(mean_HD))
+            print("-----------------------------------------")
+            HD_list_file.append(HD_list_sub)
+            HD_list_case.append(mean_HD)
+             
+        print("-----------------------------------------")
+        print("Overall Mean Dice: {}".format(np.mean(dice_list_case)))
+        print("Overall Mean HD: {}".format(np.mean(HD_list_case)))
+
+        print("Avg Dice value for per organ :")
+        buf = np.mean(dice_list_file , axis =0)
+        for idx , avg_dice_value in enumerate(buf):
+            print(f"{avg_dice_value:2.4f} ({label_key[str(idx+1)]})")
+            
+        print("Avg HD value for per organ :")
+        buf_2 = np.mean(HD_list_file , axis =0)
+        for idx , avg_HD_value in enumerate(buf_2):
+            print(f"{avg_HD_value:2.4f} ({label_key[str(idx+1)]})")
+        print("-------------------END------------------") 
+
+
+
+if __name__ == "__main__":
+    main()
+
+
